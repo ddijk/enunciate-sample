acc3c0c6ad88667e70a280523ff36638292fe40f
Dick Dijk <Dick.Dijk@ortec-finance.com>
2019-07-31 16:18:05 +0200

    demo of subtypes

---
+++ src/main/java/com/ifyouwannabecool/domain/persona/Bike.java
@@ -0,0 +1,10 @@
+package com.ifyouwannabecool.domain.persona;
+
+public class Bike {
+
+    String make;
+
+    public String getMake() {
+        return make;
+    }
+}
---
+++ src/main/java/com/ifyouwannabecool/domain/persona/Renner.java
@@ -0,0 +1,16 @@
+package com.ifyouwannabecool.domain.persona;
+
+
+public class Renner extends Persona {
+
+    String team;
+    Bike bike;
+
+    public String getTeam() {
+        return team;
+    }
+
+    public Bike getBike() {
+        return bike;
+    }
+}
